39480a25f6a76343054beca6da3d323391d365bd
OpenVED <openved.org@gmail.com>
2025-07-02 21:05:14 +0530

    HI

---
+++ Main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int age = 17;
+    
+    bool voiting_rights = false;
+
+    if (age>=18) 
+        voiting_rights = true;
+
+    return 0;
+}
